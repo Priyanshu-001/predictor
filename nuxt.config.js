b9b1f04354e0389d7a134f49f6a3207877304a93
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2023-03-10 00:53:55 +0530

    Using Niro prerender

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-routeRules:{ 
-'/':{'static':true},
-'/colleges':{'static':true}
-},
+    nitro: {
+    prerender: {
+      routes: ['/', '/colleges']
+    },
+
+
+
+
+
     experimental: { treeshakeClientOnly: false },  
     css: ['~/assets/css/main.css','vuetify/lib/styles/main.sass','@mdi/font/css/materialdesignicons.min.css'],
     build: {
