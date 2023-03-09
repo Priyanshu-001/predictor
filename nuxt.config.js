2a28128d260961506abb823e1cfbae4f501f69fd
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2023-03-09 21:39:52 +0530

    Empty

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
-routeRules:{
+routeRules:{ 
 '/':{'static':true},
 '/colleges':{'static':true}
 },
