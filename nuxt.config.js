601de9cb2570d5d58e6e74229a7b13bd646fa2cc
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2023-03-09 20:07:35 +0530

    Changing rendering mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // import vuetify from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
+routeRules:{
+'/':{'static':true},
+'/colleges':{'static':true}
+},
     experimental: { treeshakeClientOnly: false },  
     css: ['~/assets/css/main.css','vuetify/lib/styles/main.sass','@mdi/font/css/materialdesignicons.min.css'],
     build: {
