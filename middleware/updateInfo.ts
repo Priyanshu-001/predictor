5a1012ff6bc48f79ab926cc0a7947cd0c9e3f09f
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2023-03-22 22:18:44 +0530

    Update updateInfo.ts

--- middleware/updateInfo.ts
+++ middleware/updateInfo.ts
@@ -1,22 +1,18 @@
 export default defineNuxtRouteMiddleware(to=>{
-    
-    
-    
+   
     let selection = ['pool','category','pwd']
 
     if(to.name === 'predict')
         selection = ['rank','state','exam',...selection]
-    console.log(to.query)
+
     const {rank,state,exam,pool,category,pwd} = to.query
 
 
     const corrector = useCorrector()
     const {correction,correctedObj} = corrector({rank,pool,category,pwd,state,exam},selection)
 //    userInfo.value = {...userInfo.value,...correctedObj}
-    if(!correction)
-        return abortNavigation();
-  
-    return navigateTo({
+    if(correction)
+        return navigateTo({
         path:to.path,
         query:{
           ...to.query,
