11a61810234513f7e8156d6a45454acd7a7a3f6d
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2023-03-22 20:16:06 +0530

    Trial and error3

--- middleware/updateInfo.ts
+++ middleware/updateInfo.ts
@@ -9,12 +9,12 @@ export default defineNuxtRouteMiddleware(to=>{
     console.log(to.query)
     const {rank,state,exam,pool,category,pwd} = to.query
 
-    
+//     const 
     const corrector = useCorrector()
     const {correction,correctedObj} = corrector({rank,pool,category,pwd,state,exam},selection)
-   // userInfo.value = {...userInfo.value,...correctedObj}
+//    userInfo.value = {...userInfo.value,...correctedObj}
     if(!correction)
-        return;
+        return abortNavigation();
   
     return navigateTo({
         path:to.path,
