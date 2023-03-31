21fa45d59f209608c712d4c913e177215e274796
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-04-01 01:32:23 +0530

    add info regrding paper-1/2

---
+++ components/ExamsInfoBanner.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-alert type="info" variant="tonal" title="Please note" 
+                        v-if="degrees.includes('Bachelor of Architecture') || degrees.includes('Bachelor of Planning')">
+                        <template v-if="exam == 'Main'">
+                            B.Arch/B.Planning requires paper JEE-Main Paper 2.<br>
+                            B.tech and such engineering degrees require <strong> JEE-Mains Paper-1</strong>
+                        </template>
+
+                        <template v-else>
+                            For B.Planning in IIT you have qualify j
+                        </template>
+
+                        </v-alert>
+</template>
+
+<script setup>
+const {degrees,exam} = defineProps({'degrees':{
+    default:[],
+    
+},
+'exam':{
+    default:'Main'
+}
+})
+</script>
