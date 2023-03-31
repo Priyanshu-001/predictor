68587a5f650d9ba05b1089a4c9c9433b8fb7d872
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-04-01 01:33:30 +0530

    Renmae:url_cases

---
+++ pages/not_covered.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="tw-m-8 ">
+		<h1 class="tw-text-2xl  tw-font-bold "> Colleges/Counselling not in database </h1>
+		<p class="tw-text-gray-600 tw-text-xl tw-font-medium">
+            Some of the <strong>MANY </strong> colleges not in the database
+
+		</p>
+		<missed />
+		<p> There night be many more colleges where you can get in please research before taking a desicion. This is is in beta and contantly improving thanks.</p>
+	</div>
+
+</template>
+
