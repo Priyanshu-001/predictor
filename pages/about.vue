aac7bb566d69fd2227d4586398470799fa5e53b4
priyanshu-001 <26710303+Priyanshu-001@users.noreply.github.com>
2023-04-03 23:58:36 +0530

    smol fix (#8)
    * TE:9
    
    * using compsable again
    
    * added: client only to dialog
    
    * TE:10
    
    * kuch toh kiya tha
    
    * finalise: selectedCard
    
    * WIP: finalization
    
    * add info regrding paper-1/2
    
    * added: theme
    
    * /cuttoff endpoints doesnt throws error
    
    * Renmae:url_cases
    
    * finalization
    
    * WIP: finalization
    
    * WIP: smothing edges
    
    * feat: userInfo aware cards
    
    * fix: unesscary 404
    
    * chore: update
    
    * fix: heading wraps
    
    * WIP:SEO
    
    * fix: homestate tag, extra msgs
    
    * added: about
    
    * added: footer
    
    * update: doc error
    
    * fix: syntax
    
    * Delete About.vue
    
    * added: rnak notice
    
    * fix: state mismatch error
    
    * cosmetic changes
    
    * enchancement:
    
    * Added:warnings

--- pages/about.vue
+++ pages/about.vue
@@ -2,18 +2,23 @@
   <div class="about">
     <h1 class="tw-text-4xl tw-p-5 tw-font-bold">About</h1>
     <div class="flex tw-w-100 md:tw-w-3/4 tw-p-5">
-      <v-card class="col-7">
+
+      <v-card class="col-7 !tw-p-4">
         <v-card-content>
          <p> 
-Hi, thanks for visiting. This is a nuxt3 + vuetify + tailwind project. Database is a JSON file earlier it was a PSQL db.
+Hi, thanks for visiting. This is a nuxt3 + vuetify + tailwind project. Database is a JSON file earlier it was a PSQL db ¯\_(ツ)_/¯ (good decison imo).
 <br/>
 <br/>
-I created this in my second year of engineering in vue2 + vuetify and then migrated it to the current stack. <br>
-I gave my JEE in year 2019 and it didn't go well to say the least, however 4 years later I reliaze i was overreacting. <br>
-No doubt an important exam but it is idiotic to tie your self worth to JEE score. Life after JEE is bit more open ended and there will be more oppurtunities that will intrest you. <br>
+<h3><b>Background</b></h3>
+I created this in my second year of engineering using vue2 + vuetify and then migrated it to the current stack in final year. <br>
+I gave my JEE in year 2019 and it didn't go well. I was very upset but almost 4 year later I think I was overreacting. 
+No doubt an important exam but it is idiotic to tie your self worth to JEE score. 
+Life after JEE is bit more open ended and there will be more opportunities that will intrest you.  <br>
+I won't offer any advice, but I'm confident that no matter the outcome, you'll take pride in the effort you put into preparing for JEE. And remember, life after the exam will be full of interesting opportunities that you may not even be aware of yet
 <br/>
 <br/>
-Pls Report bug if uou find on and all the best for your exam and beyond.
+Pls send errors ,suggestion at jeecollegepredictor[at]gmail.com and all the best for your exam and beyond.
+
         </p>
         </v-card-content>
       </v-card>
